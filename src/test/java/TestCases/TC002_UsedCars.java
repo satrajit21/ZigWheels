8fd5e53d75dfe5bc6a608e9da8d28a9885df54f0
Saha Roy <2303539@cognizant.com>
2024-02-13 10:21:14 +0530

    Changed testcases method names and added bat file for jenkins and selenium grid

--- src/test/java/TestCases/TC002_UsedCars.java
+++ src/test/java/TestCases/TC002_UsedCars.java
@@ -9,7 +9,7 @@ import pageobjects.UsedCarsPage;
 public class TC002_UsedCars extends TC001_HomePage {
 	public UsedCarsPage up;
 	@Test(priority=3,groups= {"Master","Regression"})
-	void test5() throws IOException, InterruptedException {
+	void DisplayPopularModels() throws IOException, InterruptedException {
 		up=new UsedCarsPage(driver);
 		up.displaypopularmodels();
 		log.info("Displaying the popular models in the console");
@@ -17,7 +17,7 @@ public class TC002_UsedCars extends TC001_HomePage {
 
 	
 	@Test(priority=4,groups= {"Master","Regression"})
-	void test6() throws IOException {
+	void NavigateBackToHomePageFromUsedCarsPage() throws IOException {
 		up=new UsedCarsPage(driver);
 		up.returntohomepage();
 		log.info("Navigating back to homepage");
--- src/test/java/TestCases/TC003_UpcomingBikes.java
+++ src/test/java/TestCases/TC003_UpcomingBikes.java
@@ -9,20 +9,20 @@ import pageobjects.UpcomingBikesPage;
 public class TC003_UpcomingBikes extends TC002_UsedCars {
 	public UpcomingBikesPage up;
 	@Test(priority=6,groups= {"Master","Regression"})
-	void test7() throws IOException, InterruptedException {
+	void FilteringUpcomingModelsByBrand() throws IOException, InterruptedException {
 		up=new UpcomingBikesPage(driver);
 		up.filtermanufacturer();
 		log.info("filtering the bikes by the brand");
 	}
 	@Test(priority=7,dependsOnMethods= {"test7"},groups= {"Master","Regression"})
-	void test8() throws IOException, InterruptedException {
+	void DisplayUpcomingBikeModels() throws IOException, InterruptedException {
 		up=new UpcomingBikesPage(driver);
 		up.clickonviewmoreBtn();
 		up.displaybikedetails();
 		log.info("Displaying the bikes that are under 4 lakhs");
 	}
 	@Test(priority=8,groups= {"Master","Regression"})
-	void test9() throws IOException {
+	void NavigateBackToHomePageFromUpcomingBikesPage() throws IOException {
 		up=new UpcomingBikesPage(driver);
 		up.returntohomepage();
 		log.info("Navigating back to homepage");
